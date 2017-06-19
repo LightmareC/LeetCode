b3467c44f0c1f7d5892de6128f6dbd5b7f68510f
Cai <deathwarlock@qq.com>
2017-06-19 16:25:23 +0800

    004

--- Python/004_median_of_two_sorted_arrays.py
+++ Python/004_median_of_two_sorted_arrays.py
@@ -7,6 +7,7 @@
 #nums2 = [2]
 
 #The median is 2.0
+
 #Example 2:
 #nums1 = [1, 2]
 #nums2 = [3, 4]
@@ -20,3 +21,33 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        total = len(nums1) + len(nums2)
+        if total & 0x1:
+        	return self.findKth(nums1, nums2, total/2 + 1)
+        else:
+        	return (self.findKth(nums1, nums2, total/2 ) + self.findKth(nums1, nums2, total/2 + 1)) / 2.0
+
+    def findKth(self, nums1, nums2, k):
+     	m, n = len(nums1), len(nums2)
+     	if m > n: 
+     		return self.findKth(nums2, nums1, k) 
+     	if m == 0:
+     		return nums2[k-1]
+     	if k == 1:
+     		return min(nums1[0], nums2[0])
+
+     	i1 = min(m, k/2)
+     	i2 = k - i1
+
+     	if nums1[i1-1] < nums2[i2-1]:
+     		return self.findKth(nums1[i1:], nums2, k - i1)
+     	elif nums1[i1-1] > nums2[i2-1]:
+     		return self.findKth(nums1, nums2[i2:], k - i2)
+     	else:
+     		return nums1[i1-1]
+
+if __name__ == '__main__':
+	nums1 = [1, 2]
+	nums2 = [3, 4]
+	print(Solution().findMedianSortedArrays(nums1,nums2))
+     	
